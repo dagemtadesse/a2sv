72c12e4400cb1f01248028ea9fee5c83cb5590cc
Dagem Tadesse <dagem.seyfu@gmail.com>
2022-12-14 18:23:32 +0300

    max number of coints

--- max_number_of_coins.py
+++ max_number_of_coins.py
@@ -3,10 +3,13 @@ from typing import List
 class Solution:
     def maxCoins(self, piles: List[int]) -> int:
         piles.sort()
-        print(piles)
+        min, max, avg = 0, len(piles) - 1, len(piles) - 2
         total = 0
-        for i in range(1, len(piles), 3):
-            total += piles[i]
+        while min < max:
+            total += piles[avg]
+            min += 1
+            max -= 2
+            avg -= 2
         return total
 
 print(Solution().maxCoins([9,8,7,6,5,1,2,3,4]))
