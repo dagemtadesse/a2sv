df461af62f33209789a32e86fefd02a857e30183
Dagem Tadesse <dagem.seyfu@gmail.com>
2022-12-16 10:27:30 +0300

    reverse polish notation

---
+++ reverse_polish_notaion.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        operations = {}
+        stack = []
+
+        operations['+'] = lambda x, y: x + y
+        operations['-'] = lambda x, y: x - y
+        operations['*'] = lambda x, y: x * y
+        operations['/'] = lambda x, y: x / y
+
+        for token in tokens:
+            if not token in operations:
+                stack.append(int(token))
+            else:
+                b = stack.pop()
+                a = stack.pop()
+                stack.append(int(operations[token](a, b)))
+        return stack.pop()
+
+
+print(Solution().evalRPN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"]))
